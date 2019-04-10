d51a6ef4780c19a816018a15209421a9796ae749
David Lukes <dafydd.lukes@gmail.com>
2019-04-10 16:23:55 +0200

    0 solved

--- 0-hello-world/src/main.rs
+++ 0-hello-world/src/main.rs
@@ -1,11 +1,22 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-#[cfg(test)] mod tests;
+#[cfg(test)]
+mod tests;
 
 // FIXME: Declare a `GET /` route named `index` that returns `Hello, world!`.
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index])
+}
 
 fn main() {
     // FIXME: Ignite a rocket, mount some routes, and launch it.
+    rocket().launch();
 }
--- 0-hello-world/src/tests.rs
+++ 0-hello-world/src/tests.rs
@@ -1,9 +1,9 @@
 use super::*;
-use rocket::{local::Client, http::Status};
+use rocket::{http::Status, local::Client};
 
 #[test]
 fn hello_world() {
-    let rocket = rocket::ignite().mount("/", routes![index]);
+    let rocket = rocket();
     let client = Client::new(rocket).unwrap();
 
     let mut response = client.get("/").dispatch();
@@ -14,3 +14,13 @@ fn hello_world() {
 // FIXME: Write a test that ensures the following:
 //  * A HEAD request to `/` responds successfully.
 //  * The response has an empty body.
+
+#[test]
+fn head_test() {
+    let rocket = rocket();
+    let client = Client::new(rocket).unwrap();
+
+    let mut response = client.head("/").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert!(response.body_bytes().unwrap().is_empty());
+}
