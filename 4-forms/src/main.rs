d13e5dbf1cde267a356cb38446f0e54b62507ea4
David Lukes <dafydd.lukes@gmail.com>
2019-04-11 23:15:23 +0200

    4 solved

--- 4-forms/src/main.rs
+++ 4-forms/src/main.rs
@@ -1,8 +1,12 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+use rocket::http::RawStr;
+use rocket::request::{Form, FromFormValue, LenientForm};
 
-#[cfg(test)] mod tests;
+#[cfg(test)]
+mod tests;
 
 // FIXME: Implement the following routes:
 //
@@ -16,7 +20,12 @@
 //      Responds with a `Debug` string of the corresponding Rust structure.
 //      Requests with unknown or missing form fields are rejected with a 422
 //      error.
-//
+
+#[post("/task", format = "form", data = "<task>")]
+fn post_task(task: Form<Task>) -> String {
+    format!("{:?}", task.into_inner())
+}
+
 //   * (post_lenient_task) POST /lenient_task
 //
 //     Accepts a web form in the body with at least two fields:
@@ -28,7 +37,12 @@
 //     simply discarded. Requests with missing form fields are rejected with a
 //     422 error. Responds with a `Debug` string of the corresponding Rust
 //     structure.
-//
+
+#[post("/lenient_task", format = "form", data = "<task>")]
+fn post_lenient_task(task: LenientForm<Task>) -> String {
+    format!("{:?}", task.into_inner())
+}
+
 //   * (post_strict_task) POST /strict_task
 //
 //     Accepts a web form in the body with exactly two fields:
@@ -39,7 +53,12 @@
 //     Requests with invalid form values are rejected with a 422 error. Responds
 //     with a `Debug` string of the corresponding Rust structure.
 
-#[derive(Debug)]
+#[post("/strict_task", format = "form", data = "<strict_task>")]
+fn post_strict_task(strict_task: Form<StrictTask>) -> String {
+    format!("{:?}", strict_task.into_inner())
+}
+
+#[derive(Debug, FromForm)]
 struct Task {
     name: String,
     category: String,
@@ -48,14 +67,35 @@ struct Task {
 #[derive(Debug)]
 struct Name(String);
 
-#[derive(Debug)]
-enum Kind {
-    // FIXME: Add variants.
+impl<'v> FromFormValue<'v> for Name {
+    type Error = &'v RawStr;
+
+    fn from_form_value(form_value: &'v RawStr) -> Result<Self, Self::Error> {
+        match form_value.url_decode() {
+            Ok(string) => {
+                let length = string.len();
+                if length > 0 && length <= 128 {
+                    Ok(Name(string))
+                } else {
+                    Err(form_value)
+                }
+            }
+            _ => Err(form_value),
+        }
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, FromFormValue)]
+enum Kind {
+    Leisure,
+    Business,
+    Critical,
+}
+
+#[derive(Debug, FromForm)]
 struct StrictTask {
     name: Name,
+    #[form(field = "type")]
     kind: Kind,
 }
 
