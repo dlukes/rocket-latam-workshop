0d8029a4fc9c178f1c68a3521bd178f43f2d85ed
David Lukes <dafydd.lukes@gmail.com>
2019-04-12 21:04:32 +0200

    7 solved

--- 7-state/src/main.rs
+++ 7-state/src/main.rs
@@ -1,8 +1,13 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-#[cfg(test)] mod tests;
+use rocket::fairing::AdHoc;
+use rocket::State;
+
+#[cfg(test)]
+mod tests;
 
 struct Token(i64);
 
@@ -15,8 +20,20 @@ struct Token(i64);
 //     configuration value or the value isn't a valid `i64`, launch should be
 //     aborted.
 
+#[get("/token")]
+fn token(token: State<Token>) -> String {
+    format!("{}", token.0)
+}
+
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
+        .attach(AdHoc::on_attach("Token Config", |rocket| {
+            let token_val = match rocket.config().get_int("token") {
+                Ok(val) => val,
+                Err(_) => return Err(rocket),
+            };
+            Ok(rocket.manage(Token(token_val)))
+        }))
         .mount("/", routes![token])
 }
 
